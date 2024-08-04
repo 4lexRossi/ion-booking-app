5ceb9addd6ae74caa32ccb59e34d1c2d69e50a83
4lexrossi <devalexrossi@gmail.com>
2024-08-04 14:42:50 -0300

    initial project

---
+++ android/app/src/main/java/com/cvgen/MainActivity.java
@@ -0,0 +1,5 @@
+package com.cvgen;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.cvgen',
+  appName: 'cvGen',
+  webDir: 'www'
+};
+
+export default config;
