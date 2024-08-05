51ec6e5355db15f2702c661eea3c253d7f785b3a
4lexrossi <devalexrossi@gmail.com>
2024-08-04 21:33:33 -0300

    feat: initial app structure

---
+++ src/app/bookings/bookings.page.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-bookings',
+  templateUrl: './bookings.page.html',
+  styleUrls: ['./bookings.page.scss'],
+})
+export class BookingsPage {
+
+  constructor() { }
+
+
+
+}
---
+++ src/app/places/places.module.ts
@@ -0,0 +1,18 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+
+import { IonicModule } from '@ionic/angular';
+
+import { PlacesPageRoutingModule } from './places-routing.module';
+
+import { PlacesPage } from './places.page';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    IonicModule,
+    PlacesPageRoutingModule
+  ],
+  declarations: [PlacesPage]
+})
+export class PlacesPageModule { }
